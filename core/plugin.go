8a95035d974bddcbb94060c54740454ab5af3e07
yifenglu <yifenglu@tencent.com>
2021-07-22 19:53:23 +0800

    init project

--- core/plugin.go
+++ core/plugin.go
@@ -6,6 +6,6 @@ import (
 	_ "github.com/micro/go-plugins/registry/consul/v2"
 	//_ "github.com/micro/go-plugins/registry/etcdv3/v2"
 	_ "github.com/micro/go-plugins/transport/grpc/v2"
-	_ "github.com/micro/go-plugins/transport/quic/v2"
+	//_ "github.com/micro/go-plugins/transport/quic/v2"
 	_ "github.com/micro/go-plugins/transport/tcp/v2"
 )
